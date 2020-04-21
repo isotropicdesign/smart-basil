f92f734dce4ec2c82c1d2e2df802d606b839ebfa
isotropicdesign <63938011+isotropicdesign@users.noreply.github.com>
2020-04-21 15:01:47 -0400

    Favicon Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,19 @@ module.exports = {
     siteMetadata: require('./site-metadata.json'),
     plugins: [
         {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: `SpeedOpp`,
+              short_name: `SpeedOpp`,
+              start_url: `/`,
+              background_color: `#F7F9FB`,
+              theme_color: `#FB6100`,
+              display: `standalone`,
+              icon: `static\images\s_logo_color__hbF_icon.ico`
+            },
+          },
+
+        {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
               // The property ID; the tracking code won't be generated without it
