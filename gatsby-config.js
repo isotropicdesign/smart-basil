8bd9fbe13ed29a9dd56b70cc96f10117ea88b564
isotropicdesign <63938011+isotropicdesign@users.noreply.github.com>
2020-04-21 14:29:23 -0400

    Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,8 @@
 module.exports = {
     pathPrefix: '/',
-    siteMetadata: {
-    require('./site-metadata.json'),
-    siteUrl: `https://www.speedopp.com`,
-}
+    siteMetadata: require('./site-metadata.json'),
     plugins: [
         `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sitemap`,
         `gatsby-source-data`,
         {
             resolve: `gatsby-source-filesystem`,
@@ -20,40 +16,8 @@ module.exports = {
             options: {
                 inputFile: `${__dirname}/src/sass/main.scss`,
                 outputFile: `${__dirname}/public/assets/css/main.css`
-            },
-
-            {
-                resolve: `gatsby-plugin-google-analytics`,
-                options: {
-                  // The property ID; the tracking code won't be generated without it
-                  trackingId: "UA-159937109-3",
-                  // Defines where to place the tracking script - `true` in the head and `false` in the body
-                  head: false,
-                  // Setting this parameter is optional
-                  anonymize: true,
-                  // Setting this parameter is also optional
-                  respectDNT: true,
-                  // Avoids sending pageview hits from custom paths
-                  exclude: ["/preview/**", "/do-not-track/me/too/"],
-                  // Delays sending pageview hits on route update (in milliseconds)
-                  pageTransitionDelay: 0,
-                  // Enables Google Optimize using your container Id
-                  optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-                  // Enables Google Optimize Experiment ID
-                  experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-                  // Set Variation ID. 0 for original 1,2,3....
-                  variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-                  // Any additional optional fields
-                  sampleRate: 5,
-                  siteSpeedSampleRate: 10,
-                  cookieDomain: "speedopp.com",
-                },
-              },
-
+            },z
         },
-
-
-
         {
             resolve: `gatsby-transformer-remark`,
             options: {
